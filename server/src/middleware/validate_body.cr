d6ce98bba0715e3ebdb647238f2f68106aeacda9
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-28 06:44:47 -0300

    feat: validate body middleware

--- server/src/middleware/validate_body.cr
+++ server/src/middleware/validate_body.cr
@@ -2,23 +2,57 @@ require "kemal"
 require "json"
 require "../../config/config"
 
-class ValidateBody < Kemal::Handler
+class ValidatePostPutBody < Kemal::Handler
   only ["/travel_plans"], "POST"
   only ["/travel_plans/:id"], "PUT"
 
   def call(env)
     return call_next(env) unless only_match?(env)
-    body = env.params.json
+    begin
+      body = env.params.json
 
-    if body.empty? || body["travel_stops"].as(Array).empty?
+      if body.empty? || body["travel_stops"].as(Array).empty?
+        env.response.status_code = 400
+        error = {message: "you must provide a list of travel stops"}.to_json
+        env.response.content_type = "application/json"
+        env.response.print error
+      else
+        call_next(env)
+      end
+    rescue exception
       env.response.status_code = 400
-      error = {message: "you must provide a list of travel stops"}.to_json
+      error = {message: "you must provide a list of integers"}.to_json
       env.response.content_type = "application/json"
       env.response.print error
-    else
-      call_next(env)
     end
   end
 end
 
-add_handler ValidateBody.new
+add_handler ValidatePostPutBody.new
+
+class ValidatePatchBody < Kemal::Handler
+  only ["/travel_plans/:id/append"], "PATCH"
+
+  def call(env)
+    return call_next(env) unless only_match?(env)
+    begin
+      body = env.params.json
+
+      if body.empty?
+        env.response.status_code = 400
+        error = {message: "you must provide an integer travel stop"}.to_json
+        env.response.content_type = "application/json"
+        env.response.print error
+      else
+        call_next(env)
+      end
+    rescue exception
+      env.response.status_code = 400
+      error = {message: "you must provide an integer travel stop"}.to_json
+      env.response.content_type = "application/json"
+      env.response.print error
+    end
+  end
+end
+
+add_handler ValidatePatchBody.new
